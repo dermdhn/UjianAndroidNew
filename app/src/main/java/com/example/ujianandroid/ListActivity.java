0c53d306f81d006eaf5b08911e00a3745301ca87
Ramdon <drmdhn33@gmail.com>
2024-01-10 19:48:44 +0700

    Changes on MainActivity.java ListActivity.java activity_main.xml
    Update log
    1.Menambahkan input umur
    2.gunakan input umur untuk membuat perulangan sesuai nilai yang dimasukan
    3.gunakan input umur untuk menampilkan status :
    	-jia kurang dari 10 maka anak
    	-jika lebih dari 10 tapi kurang dari 20 maka "Remaja"
    	-jika lebih dari 20 tapi kurang dari 40 maka "Dewasa"
    	-diatas 40 maka "Tua"

--- app/src/main/java/com/example/ujianandroid/ListActivity.java
+++ app/src/main/java/com/example/ujianandroid/ListActivity.java
@@ -4,7 +4,6 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
-import android.util.Log;
 import android.widget.ArrayAdapter;
 import android.widget.ListView;
 
@@ -17,17 +16,23 @@ public class ListActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_list);
 
-        ListView lvNama = (ListView) findViewById(R.id.lvNama);
+        ListView lvNama = findViewById(R.id.lvNama);
 
-        ArrayList<String> daftar_nama = getIntent().getExtras().getStringArrayList("daftar_nama");
+        Intent intent = getIntent();
+        if (intent != null && intent.hasExtra("daftar_nama")) {
+            ArrayList<String> daftar_nama = intent.getStringArrayListExtra("daftar_nama");
 
-        if(daftar_nama.isEmpty()){
-            daftar_nama.add("data masih kosong");
+            if (daftar_nama != null && !daftar_nama.isEmpty()) {
+                ArrayAdapter<String> ad_nama = new ArrayAdapter<>(this, android.R.layout.simple_list_item_1, daftar_nama);
+                lvNama.setAdapter(ad_nama);
+            } else {
+                daftar_nama = new ArrayList<>();
+                daftar_nama.add("Data masih kosong");
+                ArrayAdapter<String> ad_nama = new ArrayAdapter<>(this, android.R.layout.simple_list_item_1, daftar_nama);
+                lvNama.setAdapter(ad_nama);
+            }
+        } else {
+            // ...
         }
-
-        ArrayAdapter<String> ad_nama = new ArrayAdapter<>(this, android.R.layout.simple_list_item_1, daftar_nama);
-
-        lvNama.setAdapter(ad_nama);
-
     }
 }
